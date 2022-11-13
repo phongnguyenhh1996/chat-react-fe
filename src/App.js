0918a1feef0d247e2ef0c947f4225e46aa8e3652
hieuhuynh2499 <hieuh5355@gmail.com>
2022-11-13 16:13:00 +0700

    install react icons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.scss';
+import { FaBeer } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <FaBeer/>
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
