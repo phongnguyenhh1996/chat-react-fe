d4b0dd84962826336203b8d3d3d5b297b1810ee6
Phong Nguyen <phongnguyenhh.1996@gmail.com>
2024-05-10 17:36:18 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import "./App.scss";
 
 import Dashboard from "./view/Dashboard";
-import {ReactComponent} from './asset/img/icons.svg'
+import { ReactComponent } from "./asset/img/icons.svg";
 
 function App() {
   return (
-    <>
-    <ReactComponent/>
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-    </Routes>
-    </>
+    <Router basename={process.env.PUBLIC_URL}>
+      <ReactComponent />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
