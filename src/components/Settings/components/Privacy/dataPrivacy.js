e54ac92e8b9a662868c2185a34aaed4d8e28c5dd
Phong Nguyen <phongnguyenhh.1996@gmail.com>
2024-05-10 17:19:41 +0700

    remove all

--- src/components/Settings/components/Privacy/dataPrivacy.js
+++
@@ -1,7 +0,0 @@
-export const dataPrivacy = [
-  { title: "Profile photo", type: "Everyone" },
-  { title: "Last seen", type: "btn" },
-  { title: "Status", type: "Everyone" },
-  { title: "Read receipts", type: "btn" },
-  { title: "Groups", type: "Everyone" },
-];
